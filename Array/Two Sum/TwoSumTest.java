e275a2d6b72d2a56a70a422186ca21cdfd807113
R-Leano <ryanleanoj@gmail.com>
2026-03-10 14:36:48 -0700

    Update TwoSumTest

--- Array/Two Sum/TwoSumTest.java
+++ Array/Two Sum/TwoSumTest.java
@@ -1,3 +1,8 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import.java.util.List;
+import static com.google.common.truth.Truth.assertThat;
+
 /**
  * TwoSum.java
  * 
@@ -6,8 +11,21 @@
  * You may assume that each input would have exactly one solution, 
  * and you may not use the same element twice.
  * You can return the answer in any order.
+ * 
+ * Case 1
+ * nums = [2, 7, 11, 15]
+ * target = 9
  */
 
 public class TwoSumTest {
     
+    @Test 
+    public void testSimpleSum() {
+        ArrayList simple = new ArrayList<>();
+        int target = 9;
+
+        simple = [2, 7]
+        
+        assertThat()
+    }
 }
