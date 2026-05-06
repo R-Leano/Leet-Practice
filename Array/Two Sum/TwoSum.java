ab41f8dfee26b56c511d046de5da85cb25584e18
R-Leano <ryanleanoj@gmail.com>
2026-05-05 21:16:59 -0700

    one pass solution, TwoSum

--- Array/Two Sum/TwoSum.java
+++ Array/Two Sum/TwoSum.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers nums and an integer target, 
  * return indices of the two numbers such that they add up to target.
@@ -8,5 +11,14 @@
 class TwoSum {
     
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
     }
 }
